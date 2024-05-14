44dde47a0a9bda026827564ea1bf196452c7bbfb
kceegram <158181757+kceegram@users.noreply.github.com>
2024-05-14 22:36:20 +0100

    new

---
+++ src/Components/HomeComp/HomeSectionOne.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+import { useState } from 'react'
+
+const HomeSectionOne = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeSectionOne
